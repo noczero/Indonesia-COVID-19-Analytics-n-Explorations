85054bcfae560fbb47890af400b25867cb907098
noczero <zeroonetm@gmail.com>
2021-04-15 22:34:12 +0700

    change time to 20:30

--- src/update_daily.py
+++ src/update_daily.py
@@ -9,7 +9,7 @@ FILE_EXTENSION = 'csv'  # export format
 SHEET_GID = '2052139453'  # get from browser url when sheet clicked
 DATA_URL = 'https://docs.google.com/spreadsheets/d/1ma1T9hWbec1pXlwZ89WakRk-OfVUQZsOCFl4FwZxzVw/export?format={}&gid={}'.format(
     FILE_EXTENSION, SHEET_GID)
-UPDATE_TIME = '19:30'
+UPDATE_TIME = '20:30'
 
 
 def run_automation():
